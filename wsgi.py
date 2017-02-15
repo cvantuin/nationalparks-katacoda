dd32e309f4f9df1b26c95e79581b060a2327e2ca
cvantuin <cvantuin@redhat.com>
2017-02-15 08:13:08 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -46,7 +46,7 @@ def format_result(entries):
     return result
 
 #DATASET_FILE = 'nationalparks.json'
-DATASET_FILE = os.getenv('DATA_FILE', 'nationalparks.json')
+DATASET_FILE = os.getenv('DATA_FILE', 'nationalparksus.json')
 
 dataset = []
 
